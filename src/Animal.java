bdef850b73ae3d59096f06f3c0d55b9c3a0dd87e
1125040-Azriel <if-25040@students.ithb.ac.id>
2026-09-09 19:45:54 +0700

    buat abstract class Animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,1 +1,43 @@
+public abstract class Animal {
+    protected String name;
+    protected int age;
+    protected String food;
 
+    public Animal(String name, int age, String food) {
+        this.name = name;
+        this.age = age;
+        this.food = food;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public abstract String printSound();
+
+    public String printData() {
+        return "Name : " + name + "\n" +
+               "Age : " + age + " tahun\n" +
+               "Food : " + food;
+    }
+}
